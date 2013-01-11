fc231c1b3dfb86cbf1d03f995111eee7bb253758
Martin Klapetek <martin.klapetek@gmail.com>
2013-01-11 18:18:13 +0100

    Forgotton header in previous commit

--- persons-delegate.h
+++ persons-delegate.h
@@ -33,6 +33,9 @@ public:
 
     void paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const;
     QSize sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const;
+
+private:
+    QString m_avatarImagePath;
 };
 
 #endif // PERSONS_DELEGATE_H
